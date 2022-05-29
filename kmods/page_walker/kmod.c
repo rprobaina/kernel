091753f3848deff7b0d25fe3959328dcf95e2ddb
Ricardo Robaina <rprobaina@gmail.com>
2022-05-29 17:18:38 -0300

    In progress: printing all Page Tables Levels' addrs and indexes

--- kmods/page_walker/kmod.c
+++ kmods/page_walker/kmod.c
@@ -9,24 +9,32 @@
 #include <linux/mm_types.h>
 #include <linux/sched/signal.h>
 #include <linux/kern_levels.h>
+#include <asm/types.h>
+#include <asm/page.h>
+#include <asm/pgtable.h>
+#include <asm/io.h>
 
 struct page *p;			/* Memory Page */
-struct task_struct *ts;	 	/* Task Struct */
-struct mm_struct *ts_mm; 	/* MM Struct   */
-
-pgd_t *pgd;	/* Page Global Directory: points to pud */
-pud_t *pud;	/* Page Upper Directory:  points to pmd */
-pmd_t *pmd;	/* Page Middle Directory: points to pte */
-pgtable_t *pte; /* Page Table Entry:	  points to the physical address */
-
-unsigned long pgd_index, pud_index, pmd_index, pte_index;	
 
 int load_func(void)
 {
-	unsigned long va; /* virtual addr we want to follow */	
-	unsigned long pa; /* physical addr found */	
+	unsigned long v_addr; /* virtual addr we want to follow */	
+	unsigned long p_addr; /* physical addr found */	
 
+	struct task_struct *ts;	 	/* Task Struct */
+	struct mm_struct *ts_mm; 	/* MM Struct   */
+
+	pgd_t *pgd;	/* Page Global Directory: points to pud */
+	p4d_t *p4d;	/* 5-level page table			*/
+	pud_t *pud;	/* Page Upper Directory:  points to pmd */
+	pmd_t *pmd;	/* Page Middle Directory: points to pte */
+	pte_t *pte; 	/* Page Table Entry:	  points to the physical address */
+
+	unsigned long _pgd_index, _p4d_index, _pud_index, _pmd_index, _pte_index;
+
+	/* allocating a memory page to follow */
 	p = alloc_pages(GFP_KERNEL, 0);
+	//p = kmalloc(1024, GFP_ATOMIC);
 	if(!p){
 		printk("Memory unavailable!\n");
 		return 1;
@@ -34,33 +42,49 @@ int load_func(void)
 	
 	printk("-------------------------------------\n");
 
-	va = (unsigned long) p;
-	printk("VIR:\t%lx\n", va);
+	/* finding the virtual address of p */
+	v_addr = (unsigned long) p;
+	printk("VIR: 0x%lx\n", v_addr);
 
+
+	/* finding the PGD and its index */
 	ts = current;
 	ts_mm = ts->mm;
 
-	pgd = ts_mm->pgd;
-	pgd_index = (va >> PGDIR_SHIFT);	
-	printk("PGD:\t%lx (index: %lx)", (unsigned long) pgd, pgd_index);
+	pgd = pgd_offset(ts_mm, v_addr);	
+	_pgd_index = pgd_index(v_addr);
+	printk("PGD: 0x%lx (index: 0x%lx)", (unsigned long) pgd, _pgd_index);
 
-	/* TODO */	
-	printk("PUD:\t%lx (index: %lx)", (unsigned long) pud, pud_index);
-	
-	/* TODO */	
-	printk("PMD:\t%lx (index: %lx)", (unsigned long) pmd, pmd_index);
+	p4d = p4d_offset(pgd, v_addr);
+	_p4d_index = p4d_index(v_addr);
+        printk("PGD: 0x%lx (index: 0x%lx)", (unsigned long) p4d, _p4d_index);
 
-	/* TODO */	
-	printk("PTE:\t%lx (index: %lx)", (unsigned long) pte, pte_index);
+	pud = pud_offset(p4d, v_addr);
+	_pud_index = pud_index(v_addr);
+	printk("P4D: 0x%lx (index: 0x%lx)", (unsigned long) pud, _pud_index);
+
+	pmd = pmd_offset(pud, v_addr);
+	_pmd_index = pmd_index(v_addr);
+	printk("PMD: 0x%lx (index: 0x%lx)", (unsigned long) pmd, _pmd_index);
 	
-	/* TODO */	
-	printk("PHY:\t%lx\n", pa);
+	pte = pte_offset_kernel(pmd, v_addr);
+	_pte_index = pte_index(v_addr);
+	printk("PTE: 0x%lx (index: 0x%lx)", (unsigned long) pte, _pte_index);
+
+	/* FIXME
+	p_addr = pte_val(*pte) & PAGE_MASK;
+	printk("PHY: 0x%lx\n", p_addr);
+
+	unsigned long api_p_addr = virt_to_phys(p);
+	printk("API PHYSICAL ADDR: 0x%lu\n", api_p_addr);
+	*/
 
 	return 0;
 }
 
 void unload_func(void)
 {
+	/* freeing the allocated page */
 	__free_pages(p, 0);
 	printk("-------------------------------------\n");
 }
